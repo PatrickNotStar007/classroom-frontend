101b576832aad612f7518f4010259fd78afaf6a4
Patrick <h.ilya2003@mail.ru>
2026-02-20 17:40:52 +0300

    implement data provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -11,12 +11,12 @@ import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import { dataProvider } from "./providers/data";
 import { BookOpen, Home } from "lucide-react";
 import { Layout } from "./components/refine-ui/layout/layout";
 import Dashboard from "./pages/Dashboard";
 import SubjectsList from "./pages/subjects/List";
 import SubjectsCreate from "./pages/subjects/Create";
+import { dataProvider } from "./providers/data";
 
 function App() {
   return (
